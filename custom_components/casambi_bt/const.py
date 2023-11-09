9738af7d0dcb415a21e4fc3133b2699b7c170c2e
Christopher Fenner <Christopher.Fenner@me.com>
2023-11-10 00:00:22 +0100

    add binary sensor platform

--- custom_components/casambi_bt/const.py
+++ custom_components/casambi_bt/const.py
@@ -2,7 +2,15 @@
 
 from typing import Final
 
+from homeassistant.const import Platform
+
 DOMAIN: Final = "casambi_bt"
+
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.LIGHT,
+    Platform.SCENE,
+]
 
 CONF_IMPORT_GROUPS: Final = "import_groups"
 
