40a135fa6a4b96cc73751e42ffad82b72a5be19f
Lukas Kempf <git@lukas-kempf.de>
2024-09-25 23:44:44 +0200

    Implement number entity for vertical control Implements Support vertical distribution #69

---
+++ custom_components/casambi_bt/number.py
@@ -0,0 +1,108 @@
+"""Support for the vertical control of Casambi compatible lights."""
+
+from __future__ import annotations
+
+from abc import ABCMeta
+import logging
+from typing import cast
+
+from CasambiBt import Group, Unit, UnitControlType, UnitState
+
+from homeassistant.components.number import NumberDeviceClass, NumberEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from . import CasambiApi
+from .const import CONF_IMPORT_GROUPS, DOMAIN
+from .entities import (
+    CasambiEntity,
+    CasambiNetworkEntity,
+    CasambiUnitEntity,
+    TypedEntityDescription,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Create the Casambi vertical entity."""
+    casa_api: CasambiApi = hass.data[DOMAIN][config_entry.entry_id]
+
+    light_entities: list[CasambiVerticalNumber] = [
+        CasambiVerticalNumberUnit(casa_api, u)
+        for u in casa_api.get_units([UnitControlType.VERTICAL])
+    ]
+
+    group_entities: list[CasambiVerticalNumber] = []
+    if config_entry.data[CONF_IMPORT_GROUPS]:
+        for g in casa_api.get_groups():
+            has_vert = False
+            for u in g.units:
+                if u.unitType.get_control(UnitControlType.VERTICAL) is not None:
+                    has_vert = True
+                    break
+            if has_vert:
+                group_entities.append(CasambiVerticalNumberGroup(casa_api, g))
+
+    async_add_entities(light_entities + group_entities)
+
+
+class CasambiVerticalNumber(CasambiEntity, NumberEntity, metaclass=ABCMeta):
+    """Defines a Casambi vertical entity base class.
+
+    This class contains common functionality for units and groups.
+    """
+
+    def __init__(
+        self, api: CasambiApi, description: TypedEntityDescription, obj: Group | Unit
+    ) -> None:
+        """Initialize a Casambi vertical entity base class."""
+
+        self._attr_device_class = NumberDeviceClass.ILLUMINANCE
+        self._attr_native_min_value = 0
+        self._attr_native_max_value = 255
+
+        self._obj: Group | Unit
+        super().__init__(api, description, obj)
+
+    async def async_set_native_value(self, value: float) -> None:
+        """Set the vertical value."""
+        await self._api.casa.setVertical(self._obj, int(value))
+
+
+class CasambiVerticalNumberUnit(CasambiVerticalNumber, CasambiUnitEntity):
+    """Defines a Casambi vertical entity."""
+
+    def __init__(self, api: CasambiApi, unit: Unit) -> None:
+        """Initialize a Casambi vertical entity."""
+
+        desc = TypedEntityDescription(key=unit.uuid, entity_type="vertical")
+
+        self._obj: Unit
+        super().__init__(api, desc, unit)
+
+    @property
+    def native_value(self) -> float | None:
+        """Get the vertical value of the unit."""
+        unit = cast(Unit, self._obj)
+        if unit.state is not None and unit.state.vertical is not None:
+            return float(unit.state.vertical)
+        else:
+            return None
+
+
+class CasambiVerticalNumberGroup(CasambiVerticalNumber, CasambiNetworkEntity):
+    """Defines a Casambi vertical entity group."""
+
+    def __init__(self, api: CasambiApi, group: Group) -> None:
+        """Initialize a Casambi vertical group entity."""
+
+        desc = TypedEntityDescription(
+            key=str(group.groudId), name=group.name, entity_type="vertical"
+        )
+        super().__init__(api, desc, group)
