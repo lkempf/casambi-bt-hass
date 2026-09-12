0197d8dd84861d9013b0c8ce64dd49a0d8507f0b
Lukas Kempf <git@lukas-kempf.de>
2026-09-12 13:19:12 +0200

    Reorganize imports

--- custom_components/casambi_bt/entities.py
+++ custom_components/casambi_bt/entities.py
@@ -1,13 +1,12 @@
 """Common functionality for entities."""
 
-import logging
 from abc import ABCMeta
 from dataclasses import dataclass
+import logging
 from typing import Final, cast
 
-from CasambiBt import Group as CasambiGroup
-from CasambiBt import Scene as CasambiScene
-from CasambiBt import Unit as CasambiUnit
+from CasambiBt import Group as CasambiGroup, Scene as CasambiScene, Unit as CasambiUnit
+
 from homeassistant.core import callback
 from homeassistant.helpers import device_registry
 from homeassistant.helpers.entity import DeviceInfo, Entity, EntityDescription
